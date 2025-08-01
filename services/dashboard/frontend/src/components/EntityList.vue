5ed12493adc650063883a07d659697ba5bab8293
Björn Otto <bjoern.otto@ovgu.de>
2025-08-01 15:09:35 +0200

    Add entity list

---
+++ services/dashboard/frontend/src/components/EntityList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {defineProps} from 'vue'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+defineProps({
+    'entities': Array
+})
+</script>
+
+<template>
+<DataTable :value="entities">
+    <Column field="id" header="Entity"></Column>
+    <Column field="type" header="Type"></Column>
+</DataTable>
+</template>
--- services/dashboard/frontend/src/components/Map.vue
+++ services/dashboard/frontend/src/components/Map.vue
@@ -1,25 +1,28 @@
 <script setup>
 import { VMap, VMapOsmTileLayer, VMapGoogleTileLayer, VMapArcGisAeroTileLayer, VMapZoomControl, VMapMarker, VMapAttributionControl } from 'vue-map-ui';
-import { onMounted, getCurrentInstance, ref } from 'vue'
-const markers = ref([])
-const bounds = ref(null)
+import { onMounted, defineProps, ref, computed, watch } from 'vue'
+const props = defineProps({
+    'entities': Array,
+})
 
-async function updateMarkers() {
-    const response = await fetch('/entities');
-    const entities = await response.json()
-    markers.value = entities.map( (i) => i['location']['value']['coordinates'])
+const markers = computed(() =>
+    props.entities.map((i) => i['location']['value']['coordinates'])
+)
+
+const bounds=ref(null);
+
+// move map to the first set of valid markers
+watch(markers, () => {
+    if(bounds.value) return
     const group = L.featureGroup(markers.value.map(marker =>
         L.marker([marker[0], marker[1]])
     ));
-    bounds.value = group.getBounds().pad(0.2);
-}
-
-onMounted(async (i) => {
-    updateMarkers();
-    setInterval(updateMarkers, 1_000);
+    const groupBounds = group.getBounds()
+    if (groupBounds.isValid()) {
+        bounds.value = groupBounds.pad(.2)
+    }
 })
 </script>
-
 
 <template>
     <VMap style="width: 100%; height: 100%;" :bounds="bounds">
